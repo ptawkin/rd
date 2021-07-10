3914e60364867a879f6321eabc5176bce5f1f7c0
Jen <gk_ln@mail.ru>
2021-07-10 17:18:46 +0300

    first commit

---
+++ src/blocks/MainCompnent/MainComponent.js
@@ -0,0 +1,14 @@
+import './MainComponent.css';
+
+function MainComponent() {
+  return (
+    <div className={'main-component'}>
+      ddddd
+      <div className={'main-component__content'}>
+        dsfdsf
+      </div>
+    </div>
+  );
+}
+
+export default MainComponent;
---
+++ src/blocks/Page/Page.js
@@ -0,0 +1,13 @@
+import MainComponent from "../MainCompnent/MainComponent";
+
+import './Page.css';
+
+function Page() {
+    return (
+        <div className={'page'}>
+            <MainComponent />
+        </div>
+    );
+}
+
+export default Page;
